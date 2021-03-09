bdc0c5573e1b3713df84f8c52616ac62d5c415b6
deepak <deepbharty@gmail.com>
2021-03-09 18:25:12 +0530

    Login class File added

---
+++ src/test/java/testCases/UserLoginTest.java
@@ -0,0 +1,13 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class UserLoginTest {
+
+	
+	@Test
+	
+	public void login() {
+		System.out.println("Login successfully");
+	}
+}
