5bd7f1c9e3f0f7638ff97ab81bd62a67dc374f5d
deepak <deepbharty@gmail.com>
2021-03-10 21:42:39 +0530

    Added user file

--- src/test/java/testCases/UserLoginTest.java
+++ src/test/java/testCases/UserLoginTest.java
@@ -5,19 +5,32 @@ import org.testng.annotations.Test;
 public class UserLoginTest {
 
 	
-	@Test
+	@Test(priority=0)
 	
 	public void login() {
 		System.out.println("Login successfully");
 	}
 	
-	@Test
+	@Test(priority=1)
 	
 	public void homePage() {
 		System.out.println("Navigate to Homepage");
 	}
 	
-	@Test
+@Test(priority=2)
+	
+	public void addUser() {
+		System.out.println("Navigate to Homepage");
+	}
+@Test(priority=3)
+
+public void viewUser() {
+	System.out.println("Navigate to Homepage");
+}
+
+
+	
+	@Test(priority=3)
 	
 	public void logout() {
 		System.out.println("Logout successfully");
